168f8070d0698dc77f06463a18ca6ef561cc72c1
Yan Wang <yawanng@google.com>
2019-08-27 20:14:09 -0700

    startop: Rename the camera trace file to a more general name (package only). Different testing framework use different activity.
    This is for temporary testing purpose.
    
    Test: make
    Bug: 138755582
    Bug: 139831359
    Change-Id: I07013c422f1f69335d263a33bffac44485ce791a

--- src/manager/event_manager.cc
+++ src/manager/event_manager.cc
@@ -94,28 +94,20 @@ struct AppComponentName {
    * '%' is encoded into ^^
    *
    * Two purpose:
-   * 1. This allows the component name to be used as a file name
+   * 1. This allows the pacakge name to be used as a file name
    * ('/' is illegal due to being a path separator) with minimal
    * munging.
-   * 2. This allows the component name to be used in .mk file because
+   * 2. This allows the package name to be used in .mk file because
    * '%' is a special char and cannot be easily escaped in Makefile.
    *
    * This is a workround for test purpose.
+   * Only package name is used because activity name varies on
+   * diffferent testing framework.
    * Hopefully, the double "@@" and "^^" are not used in other cases.
    */
-
-  // "com.foo.bar@@.A^^25" -> {"com.foo.bar", ".A%"}
-  static AppComponentName FromMakFileSafeEncodedString(const std::string& s) {
-    std::string cpy = s;
-    Replace(cpy, "@@", "/");
-    Replace(cpy, "^^", "%");
-
-    return FromString(cpy);
-  }
-
-  // {"com.foo.bar", ".A%"} -> "com.foo.bar@@.A^^"
-  std::string ToMakeFileSafeEncodedString() const {
-    std::string s = ToString();
+  // {"com.foo.bar", ".A%"} -> "com.foo.bar"
+  std::string ToMakeFileSafeEncodedPkgString() const {
+    std::string s = package;
     Replace(s, "/", "@@");
     Replace(s, "%", "^^");
     return s;
@@ -312,7 +304,7 @@ struct AppLaunchEventState {
     // This is changed from "/data/misc/iorapd/" for testing purpose.
     // Should be restored b/139831359.
     std::string file_path = "/product/iorap-trace/";
-    file_path += component_name.ToMakeFileSafeEncodedString();
+    file_path += component_name.ToMakeFileSafeEncodedPkgString();
     file_path += ".compiled_trace.pb";
 
     prefetcher::TaskId task{id, std::move(file_path)};
